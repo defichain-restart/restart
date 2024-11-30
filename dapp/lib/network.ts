b56e89d60c774a1a6f57b2de41516cfe2f8296ea
defichain-restart <defichainrestart@proton.me>
2024-11-30 21:32:16 +0000

    back to mainnet

--- dapp/lib/network.ts
+++ dapp/lib/network.ts
@@ -1,13 +1,13 @@
 import { MetaMaskChainInterface } from "../hooks/metamask.hook";
 
 export const main: MetaMaskChainInterface = {
-  chainId: 1131,
+  chainId: 1130,
   chainName: "MetaChain",
   nativeCurrency: {
     name: "DFI",
     symbol: "DFI",
     decimals: 18,
   },
-  rpcUrls: ["https://dmc.mydefichain.com/testnet"],
-  blockExplorerUrls: ["https://testnet-dmc.mydefichain.com:8441/"],
+  rpcUrls: ["https://dmc.mydefichain.com/mainnet"],
+  blockExplorerUrls: ["https://mainnet-dmc.mydefichain.com:8441/"],
 };
